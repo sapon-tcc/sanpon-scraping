5650c870d589695857b24f1351b1f2992bd83546
DanielNery <danielpontesnery@gmail.com>
2023-10-11 22:16:11 -0300

    feat: Adicionando mongo no compose.

--- app/mongo.py
+++ app/mongo.py
@@ -5,5 +5,5 @@ class MongoDB():
     def __init__(self):
         self.client = pymongo.MongoClient("mongodb://localhost:27017/")
         self.db = self.client["sapon"]
-        self.colecao = self.db["opnioes"]
+        self.colecao = self.db["opinioes"]
         
